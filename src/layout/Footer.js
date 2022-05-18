b389bbc14d40fa5a017ce9e478bce0974d7d8189
reenav01 <reenavh@sjcem.edu.in>
2022-05-18 22:34:54 +0530

    Manifest Changes

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -17,11 +17,11 @@ const Footer = () => {
         className="sticky-bottom footer text-uppercase"
         >
         
-        <AiFillLinkedin className='social_icon' />
-        <AiFillInstagram className='social_icon' />
-        <AiFillGithub className='social_icon' />
-        <AiFillTwitterCircle className='social_icon' />
-        <AiFillRedditCircle className='social_icon' />
+        <a href='https://www.linkedin.com/in/reenav-hemaria-783758202/'><AiFillLinkedin className='social_icon' /></a>
+        <a href='https://www.instagram.com/r_e_e_9/'><AiFillInstagram className='social_icon' /></a>
+        <a href='https://github.com/reenav01'><AiFillGithub className='social_icon' /></a>
+        <a href='https://twitter.com/HemariaReenav'><AiFillTwitterCircle className='social_icon' /></a>
+        <a href='https://www.reddit.com/u/ariameh_naveer?utm_medium=android_app&utm_source=share'><AiFillRedditCircle className='social_icon' /></a>
          
         
         <span className='footer_text float-end'>&copy; 2020 - All Rights Reserved</span>
